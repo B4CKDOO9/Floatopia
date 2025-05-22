3e41ddaba1c67d71f7a3fa4bf242313f3e83117b
B4CKDOO9 <smolecprofesional@gmail.com>
2025-05-22 23:01:49 +0200

    did a thing or two?
    biggg coccks

---
+++ Floatopiaaa/src/js/index.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\Smolec\Documents\GitHub\Floatopia\Floatopiaaa\src\js\index.js
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    menuToggle.addEventListener("click", () => {
+        dropdownMenu.classList.toggle("open");
+    });
+});
