1dd369dd5900aa5539b0e995f7817fc99354350b
B4CKDOO9 <smolecprofesional@gmail.com>
2025-05-28 22:44:40 +0200

    Countdown to sumeer
    HELL YEAH BABY

--- Floatopiaaa/src/js/index.js
+++ Floatopiaaa/src/js/index.js
@@ -6,4 +6,30 @@ document.addEventListener("DOMContentLoaded", () => {
     menuToggle.addEventListener("click", () => {
         dropdownMenu.classList.toggle("open");
     });
+
+    // Call the countdown function here
+    CountdownToSummer();
 });
+
+function CountdownToSummer() {
+    var countDownDate = new Date("Jun 13, 2025 24:00:00").getTime();
+
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
